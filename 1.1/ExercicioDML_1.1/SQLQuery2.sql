5b6f949cb66edd6abf3fe645de3b650846fa918e
Kamille Pereira Milo <47766876825@info.local>
2023-08-03 08:58:04 -0300

    Scripts DML E DDL Finalizados

--- 1.1/ExercicioDML_1.1/SQLQuery2.sql
+++ 1.1/ExercicioDML_1.1/SQLQuery2.sql
@@ -10,7 +10,42 @@ VALUES (1,'julia@mail.com')
 INSERT INTO Telefone(IdPessoa,Numero)
 VALUES (1,'112546897')
 
+
+--DQL
+
+--Seleciona as colunas que irão aparecer
+--AS= Altera o nome da coluna.
+SELECT 
+Pessoa.Nome AS Cliente,
+Email.Endereço AS Email,
+Pessoa.CNH,
+Telefone.Numero AS Telefone
+
+--FROM é tabela que vai ser Referenciada.  
+--Traz todas as pessoas cadastradas mesmo tendo ou não tendo correspondecia(Dados) 
+--nas tabelas email , ou telefone.
+FROM
+Pessoa
+
+--JOIN= Traz apenas os cadrastros que tiverem os dados das tabelas correspondentes(Preenchidos); EMail e Telefone.
+
+--LEFT JOIN = dá preferencia para a tabela da esquerda (Pessoa),e traz todos os cadrastros dessa tabela
+--mesmo sem os outros dados correspondentes(Email e Telefone).
+
+--RIGTH JOIN = Dá preferencia para a tabela da direita (Telefone)
+
+ LEFT JOIN 
+Email ON Pessoa.IdPessoa = Email.IdPessoa
+
+ LEFT JOIN 
+Telefone ON Pessoa.IdPessoa = Telefone.IdPessoa
+
+WHERE Telefone.Numero IS NULL
+
 SELECT * FROM Pessoa
-SELECT * FROM Email
 SELECT * FROM Telefone
+SELECT * FROM Email
+
+
+
 
--- Atividades_DML/ExercicioDML_1.4/SQLQuery6.sql
+++ Atividades_DML/ExercicioDML_1.4/SQLQuery6.sql
@@ -8,9 +8,10 @@ VALUES ('R&B')
 
 --Caso esteja ativo = 1
 --Caso Não =0
+
 INSERT INTO Albuns(IdArtista,Titulo,DataLancamento,QDeMinutos,Ativo)
 VALUES (3,'Plastich Beach','23/11/2015',190,0)
 
-SELECT Nome FROM Artistas;
-SELECT Titulo FROM Albuns;
+SELECT *  FROM Artistas;
+SELECT *FROM Albuns;
 SELECT * FROM Estilos;
